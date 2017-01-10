debee3c3dddb7d509e61f4313cd33d5ee0673991
uzquiano <mgu1@cornell.edu>
2017-01-10 17:42:17 -0400

    Added UI extension example for a theme

---
+++ ui-extensions/arrested-development-theme/index.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+    var UI = require("ui");
+
+    // register the theme: "arrested-development-theme"
+    UI.registerTheme({
+        "key": "arrested-development-theme",
+        "title": "Arrested Development Theme",
+        "module": "_modules/arrested-development-theme/theme"
+    });
+
+});
