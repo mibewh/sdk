a90a1b4cdabb565a29b6101044beef5cda0a2e82
uzquiano <mgu1@cornell.edu>
2017-02-18 22:52:10 -0500

    Updated web hook server to simplify it a bit and add an echo handler for web hook action testing

--- webhook-server/README.md
+++ webhook-server/README.md
@@ -1,28 +1,57 @@
-# Cloud CMS SDK - Workflow webhook server sample
+# Cloud CMS Sample Web Hook Server
 
-This directory contains a sample node.js, express application which can recieve and respond to Cloud CMS workflow webhook such as this:
+This directory provides a sample Node.js-based web server that can be used to demonstrate web hooks with Cloud CMS.
+It provides web hook endpoints for Web Hook actions driven from policy rules as well as workflow.
 
+For example, in a Cloud CMS workflow, you might invoke a web hook like this:
+
+````
 "leave": [{
     "type": "webhook",
     "config": {
-        "url": "https://ccms-webhook-server.herokuapp.com/api/taskValidationRandomFail"
+        "url": "https://ccms-webhook-server.herokuapp.com/taskValidationRandomFail"
     }
 }]
+````
 
-## webhook-server
-This is an npm enabled nodejs application. To build (resolves node dependencies) and run the app:
+Where you may have deployed `server.js` to an imaginary endpoint running at `https://ccms-webhook-server.herokuapp.com`.
 
-* npm install
-* npm run
+Similarly, within Cloud CMS, you may configure a Web Hook rule action like this:
 
-When run using npm run or from node.js server the port it will listen on defaults to 8080. If deployed to a hosting provider that supplies a process environment (process.env.PORT) such as heroku.com then the port will likely be 80.
+````
+{
+    "url": "https://ccms-webhook-server.herokuapp.com/echo",
+    "method": "POST"
+}
+````
+
+## Building
+
+To build, simple run:
+
+    npm install
+    node server
+
+By default, this runs on port 3000.  Use the `PORT` environment variable to adjust.
 
 The API endpoint list can be found here:
-  http://localhost:8080/api
+  http://localhost:3000/
 
-Endpoints:  
-* "GET/POST: /api/taskValidationAlwaysSucceed  Always returns HTTP 200 and { 'message': 'success' }",
-* "GET/POST: /api/taskValidationAlwaysFail  Always returns HTTP 400 and { 'message': 'fail' }",
-* "GET/POST: /api/taskValidationRandomFail  Randomly succeeds roughly 50% of the time it is called. Fails the other 50%"
+## Endpoints
 
-Workflow webhook calls will always be a POST but this server responds to any HTTP method (GET for example).
+The following endpoints are available:
+
+    POST /taskValidationAlwaysSucceed  
+    Always returns HTTP 200 and { 'message': 'success' }
+    
+    POST /taskValidationAlwaysFail  
+    Always returns HTTP 400 and { 'message': 'fail' }
+    
+    POST /taskValidationRandomFail  
+    Randomly succeeds roughly 50% of the time it is called. Fails the other 50%
+    
+    POST /echo 
+    Echoes request back with 200
+
+Cloud CMS recommends using POST for all of your web hook endpoints though it also allows for GET, PUT and DELETE.
+As such, these examples default to using POST.
--- webhook-server/server.js
+++ webhook-server/server.js
@@ -1,45 +1,105 @@
-// server.js
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express    = require('express');
+var app        = express();
 var bodyParser = require('body-parser');
+var moment     = require("moment");
+
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var describeRequest = function(req)
+{
+    var obj = {};
+    obj.method = req.method;
+    obj.path = req.path;
+    if (req.query) {
+        obj.query = req.query;
+    }
+    if (req.headers) {
+        obj.headers = req.headers;
+    }
+    if (req.body) {
+        obj.body = req.body;
+    }
 
-var router = express.Router();              // get an instance of the express Router
-router.all('/', function(req, res) {                        
-  console.log('req ' + req.ref);
-  res.status(200).json({ message: 'success', endpoints: [
-      "GET/POST: /api/taskValidationAlwaysSucceed  Always returns HTTP 200 and { 'message': 'success' }",
-      "GET/POST: /api/taskValidationAlwaysFail  Always returns HTTP 400 and { 'message': 'fail' }",
-      "GET/POST: /api/taskValidationRandomFail  Randomly succeeds roughly 50% of the time it is called. Fails the other 50%"
-      ]
-   });   
-});
-router.all('/taskValidation', function(req, res) {
-  // do something useful here. until then always succeed
-  res.status(200).json({ message: 'success' });   
-});
-router.all('/taskValidationRandomFail', function(req, res) {
-  // randomly fail 50% of the time
-  var n = Math.floor(Math.random()*100) < 50;
-  if(Math.floor(Math.random()*10) > 5) {
-    res.status(400).json({ message: 'fail' });   
-  } else {
-    res.status(200).json({ message: 'success'});   
-  }
-});
-router.all('/taskValidationAlwaysSucceed', function(req, res) {
-  res.status(200).json({ message: 'success' });   
-});
-router.all('/taskValidationAlwaysFail', function(req, res) {
-  res.status(400).json({ message: 'fail' });   
+    obj.timestamp = moment().format("MM-DD-YYYY HH:mm:ss.ms");
+
+    return obj;
+};
+
+// logs request to stdout for development use
+var logRequest = function(req)
+{
+    var obj = describeRequest(req);
+
+    console.log(obj.timestamp + " " + obj.method + " " + obj.path + ": " + JSON.stringify(obj, null, 2));
+};
+
+// log all requests
+app.use(function(req, res, next) {
+    logRequest(req);
+    next();
 });
 
-app.use('/api', router);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// ROUTES
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// START THE SERVER
+var router = express.Router();
+app.use('/', router);
+
+router.get('/', function(req, res)
+{
+    res.status(200).json({
+        message: 'success',
+        endpoints: [
+            "GET/POST: /taskValidationAlwaysSucceed  Always returns HTTP 200 and { 'message': 'success' }",
+            "GET/POST: /taskValidationAlwaysFail  Always returns HTTP 400 and { 'message': 'fail' }",
+            "GET/POST: /taskValidationRandomFail  Randomly succeeds roughly 50% of the time it is called. Fails the other 50%",
+            "GET/POST: /echo  Echoes request back with 200"
+        ]
+    });
+});
+
+router.post('/taskValidation', function(req, res)
+{
+    // do something useful here. until then always succeed
+    res.status(200).json({ message: 'success' });
+});
+
+router.post('/taskValidationRandomFail', function(req, res)
+{
+    // randomly fail 50% of the time
+    var n = Math.floor(Math.random()*100) < 50;
+    if(Math.floor(Math.random()*10) > 5) {
+        res.status(400).json({ message: 'fail' });
+    } else {
+        res.status(200).json({ message: 'success'});
+    }
+});
+
+router.post('/taskValidationAlwaysSucceed', function(req, res)
+{
+    res.status(200).json({ message: 'success' });
+});
+
+router.post('/taskValidationAlwaysFail', function(req, res)
+{
+    res.status(400).json({ message: 'fail' });
+});
+
+router.post("/echo", function(req, res)
+{
+    res.status(200).json(describeRequest(req));
+});
+
+
+///////
+
+
 app.listen(port);
-console.log('Running. Listen port: ' + port);
+console.log("Cloud CMS Example Web Hook Server Running");
+console.log("Running on port: " + port);
