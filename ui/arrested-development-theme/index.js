0db975ed736fd4dfbb6c85e528ae61ff6d72b189
Harry Moore <harry.moore@cloudcms.com>
2018-02-06 10:52:03 -0500

    load module from dynamically deployed path

--- ui/arrested-development-theme/index.js
+++ ui/arrested-development-theme/index.js
@@ -1,12 +1,13 @@
 define(function(require) {
 
     var UI = require("ui");
+    var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
 
     // register the theme: "arrested-development-theme"
     UI.registerTheme({
         "key": "arrested-development-theme",
         "title": "Arrested Development Theme",
-        "module": "_modules/arrested-development-theme/theme"
+        "module": moduleId + "/theme.js"
     });
 
 });
