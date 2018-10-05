77b1299403c12c8563915170d72bba87fc0608e5
cl <claire.li@cloudcms.com>
2018-10-05 15:12:24 -0400

    Add thinglink as a sample for having multiple tags for embedded media

---
+++ ui/modules/editors/embedded-media/media-types/thinglink-link.js
@@ -0,0 +1,61 @@
+define(function (require, exports, module) {
+
+    var Registry = require("medialink-builder/medialink-registry");
+    var MediaLinkBuilder = require("medialink-builder/medialink-builder");
+
+    return Registry.registerMediaLinkClass(MediaLinkBuilder.extend({
+
+        /**
+         * @override
+         */
+        getSchema: function () {
+            return {
+
+            };
+        },
+
+        /**
+         * @override
+         */
+        getOptions: function () {
+            return {
+
+            };
+        },
+
+        /**
+         * @override
+         */
+        generateLink: function (control, template, callback) {
+            // main element
+            var el = MediaLinkBuilder.prototype.generateLink(control, template, callback);
+            var mediaId = control.childrenByPropertyId["mediaId"].getValue();
+            var src = "//cdn.thinglink.me/api/image/" + mediaId + "/1024/10/scaletowidth#tl-" + mediaId + ";";
+            el.attr("src", src);
+            el.attr("style", "max-width: 100%;");
+            el.attr("className", "alwaysThinglink");
+
+            // div tag
+            var div = document.createElement('div');
+            div.innerHTML = "<script async charset=\"utf-8\" src=\"//cdn.thinglink.me/jse/embed.js\"></script>";
+            el.push(div);
+
+            // script tag
+            var script = document.createElement('script');
+            script.setAttribute('type', 'text/javascript');
+            script.setAttribute('src', 'http://mysite/my.js');
+            el.push(script);
+
+            callback(null, el);
+        },
+
+        /**
+         * @override
+         */
+        canHandle: function (mediaType) {
+            return mediaType == "thinglink";
+        }
+
+    }));
+
+});
