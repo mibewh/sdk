eccf9ab77323f09d989bc18a91f80878819e7bbf
Michael Whitman <michael.whitman@cloudcms.com>
2018-12-11 10:24:20 -0500

    basic server code

---
+++ sample-bookstore/app.js
@@ -0,0 +1,71 @@
+var server = require("cloudcms-server/server");
+
+var routes = function (app, callback)
+{
+    app.get("/api/books", function(req, res) {
+        req.branch(function(err, branch) {
+            branch.trap(function(err) {
+                return res.status(500).json(err);
+            }).queryNodes({
+                _type: "store:book"
+            }).each(function() {
+                this.imageUrl = '/proxy/repositories/' + this.getRepositoryId() + '/branches/' + this.getBranchId() + '/nodes/' + this._doc + '/attachments/default';
+                this.authorTitle = this.author.title;
+            }).then(function() {
+                res.status(200).json(this);
+            });
+        });
+    });
+
+    app.get("/api/authors", function(req, res) {
+        req.branch(function(err, branch) {
+            branch.trap(function(err) {
+                return res.status(500).json(err);
+            }).queryNodes({
+                _type: "store:author"
+            }).each(function() {
+                this.imageUrl = '/proxy/repositories/' + this.getRepositoryId() + '/branches/' + this.getBranchId() + '/nodes/' + this._doc + '/attachments/default';
+            }).then(function() {
+                res.status(200).json(this);
+            });
+        });
+    });
+
+    callback();
+};
+
+server.routes(routes);
+
+server.start({
+    "setup": "single",
+    "welcome": {
+        "enabled": true,
+        "file": "index.html"
+    },
+    "wcm": {
+        "enabled": true,
+        "cache": false
+    },
+    "cache": {
+        "enabled": true
+    },
+    "autoRefresh": {
+        "log": true
+    },
+    "perf": {
+        "enabled": true,
+        "paths": [{
+            "regex": "/static/.*",
+            "cache": {
+                "seconds": 300
+            }
+        }]
+    }
+});
+
+
+
+
+
+
+
